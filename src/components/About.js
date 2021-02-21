64c709deebede7453dcc0f07274cd940d9a0c15b
Alex Marz <alexhmarz@gmail.com>
2021-02-21 15:15:23 -0500

    Done.

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import { image } from "../data/data";
 
 function About() {
   return (<div id='about'>
+    <h2>About Me</h2>
     <p>
       This is the page that I created myself using what I have learned in React this far. 
     </p>
